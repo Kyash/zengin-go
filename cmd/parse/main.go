aba321f347856bab21022e779b57b21d84e34e06
utzuro <utzuro@pm.me>
2023-12-19 10:43:30 +0900

    create cmd tool for testing parsing locally

---
+++ cmd/parse/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	zengin "Kyash/zengin/parser"
+	"log"
+	"os"
+)
+
+func main() {
+	var fileName string
+
+	if len(os.Args) > 2 {
+		log.Fatal("too many arguments")
+	}
+
+	if len(os.Args) < 2 {
+		fileName = "zengin.txt"
+	} else {
+		fileName = os.Args[1]
+	}
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	records, err := zengin.ParseFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, record := range records {
+		println(record)
+	}
+
+}
