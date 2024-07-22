dc22a63a246b0634f74c84a50c1f13270ec7fccb
utzuro <utzuro@pm.me>
2024-07-22 12:14:48 +0900

    provide way to get parsed fields directly

--- zengin.go
+++ zengin.go
@@ -2,24 +2,41 @@ package zengin
 
 import (
 	zengin "github.com/Kyash/zengin-go/internal"
+	"github.com/Kyash/zengin-go/types"
 )
 
-// ParseToString /*
-// Parse 全銀 format file and return rows
-// which consists of fields in this order:
-// 振込名義人,振込日,金融機関コード,支店コード,科目,口座番号,口座名義人,金額
-func ParseToString(reader zengin.Reader) ([][]string, error) {
+// Parse Zengin format file and return rows with all fields
+func Parse(reader zengin.Reader) ([]types.Transfer, error) {
+	transfers, err := zengin.Parse(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	return transfers, nil
+}
+
+// ToCSV
+// Parse Zengin format file and return a csv like table with field names as below
+// SenderName,TransferDate,BankCode,BranchCode,AccountType,AccountNumber,AccountName,Amount
+func ToCSV(reader zengin.Reader) ([][]string, error) {
 
 	transfers, err := zengin.Parse(reader)
 	if err != nil {
 		return nil, err
 	}
 
-	return zengin.ConvertToTable(transfers), nil
+	return zengin.ToTable(transfers), nil
 }
 
-// Parse /*
-// Parse 全銀 format file and return rows with all fields
-func Parse(reader zengin.Reader) {
-	// TODO: implement
+// ToCSVJa
+// Parse Zengin format file and return a csv like table with field names as below
+// 振込名義人,振込日,金融機関コード,支店コード,科目,口座番号,口座名義人,金額
+func ToCSVJa(reader zengin.Reader) ([][]string, error) {
+
+	transfers, err := zengin.Parse(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	return zengin.ToTableJa(transfers), nil
 }
